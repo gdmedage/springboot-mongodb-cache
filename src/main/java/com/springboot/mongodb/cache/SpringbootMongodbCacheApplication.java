a2abc6f69d9f502afe98faf66bd497a5f3bda5b2
gdmedage <gdmedage@gmail.com>
2021-09-28 19:24:09 +0530

    First Commit

---
+++ src/main/java/com/springboot/mongodb/cache/SpringbootMongodbCacheApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.mongodb.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/mongodb/cache/SpringbootMongodbCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mongodb.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
